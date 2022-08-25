baa96b6892036c82b339a365eaed581d89354e11
Carlo1891 <carlo.18@live.it>
2022-08-25 16:05:01 +0200

    Initial commit

---
+++ data.php
@@ -0,0 +1,73 @@
+
+albums = [
+    [
+        'title' => 'New Jersey',
+        'author' => 'Bon Jovi',
+        'year' => "1988",
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+        'genre' => 'Rock'
+    ],
+    [
+        'title' => 'Live at Wembley 86',
+        'author' => 'Queen',
+        'year' => "1992",
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Ten\'s Summoner\'s Tales',
+        'author' => 'Sting',
+        'year' => "1993",
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+        'genre' => 'Pop'
+    ],
+    [
+        'title' => 'Steve Gadd band',
+        'author' => 'Steve Gadd Band',
+        'year' => "2018",
+        'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+        'genre' => 'Jazz'
+    ],
+    [
+        'title' => 'Brave new World',
+        'author' => 'Iron Maiden',
+        'year' => "2000",
+        'poster' => 'https://upload.wikimedia.org/wikipedia/en/0/03/Iron_Maiden_-_Brave_New_World.jpg',
+        'genre' => 'Metal'
+    ],
+    [
+        'title' => 'One more car, one more rider',
+        'author' => 'Eric Clapton',
+        'year' => "2002",
+        'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+        'genre' => 'Rock'
+    ],
+    [
+        'title' => 'Led Zeppelin I',
+        'author' => 'Led Zeppelin',
+        'year' => "1969",
+        'poster' => 'https://m.media-amazon.com/images/I/51SUWsbmnHL._AC_SX466_.jpg',
+        'genre' => 'Rock'
+    ],
+    [
+        'title' => 'Plastic Beach',
+        'author' => 'Gorillaz',
+        'year' => "2010",
+        'poster' => 'https://m.media-amazon.com/images/I/81EP2fxHLRL._AC_SX466_.jpg',
+        'genre' => 'Hip Hop'
+    ],
+    [
+        'title' => 'Evangelion Finally',
+        'author' => 'Evangelion',
+        'year' => "2021",
+        'poster' => 'https://m.media-amazon.com/images/I/71wtTaNutIS._AC_SX450_.jpg',
+        'genre' => 'Soundtrack'
+    ],
+    [
+        'title' => 'Moderat',
+        'author' => 'Moderat',
+        'year' => "2009",
+        'poster' => 'https://m.media-amazon.com/images/I/512hW0tgA+L._AC_SX450_.jpg',
+        'genre' => 'Elettronica'
+    ],
+];
